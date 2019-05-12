bbf0e4c96c78596e2897cdd2b4e4fd3c73ec8321
estherpato <estherpato@gmail.com>
2019-05-12 19:12:12 +0200

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
 import { CardComponent } from './card/card.component';
 import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent,
     CardComponent,
     FormComponent
   ],
